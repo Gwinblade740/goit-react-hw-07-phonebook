cb01579d5ffc12d2157fed99f0fa354fb1e16f40
Gwinblade740 <61421307+Gwinblade740@users.noreply.github.com>
2023-07-02 01:43:16 +0300

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { fetchContacts } from '../redux/Operation';
+
+import { FormComponent } from 'components/FormComponent/FormComponent';
+import ListComponent from './ListComponent/ListComponent';
+import Filter from 'components/Filter/Filter';
+
 export const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="container">
+      <h1>Phonebook</h1>
+      <FormComponent></FormComponent>
+      <h2>Contacts</h2>
+      <Filter></Filter>
+
+      <ListComponent></ListComponent>
     </div>
   );
 };
